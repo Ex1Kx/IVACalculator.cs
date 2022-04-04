cb1bfe5389da18018bc572537a46f6afda8ced88
Ex1Kx <thenewestekigoh@gmail.com>
2022-04-04 13:21:13 -0500

    [Ex1] Added The Project

---
+++ Calculadora/CalculadoradeImpuestos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculadora
+{
+    public class CalculadoradeImpuestos
+    {
+        public decimal ObtenerIVA(decimal montoReserva)
+        {
+            const decimal porcentaje = 0.19m;
+            return porcentaje * montoReserva;
+        }
+        public decimal ObtenerISH(decimal montoReserva, decimal porcentaje)
+        {
+            return montoReserva * porcentaje;
+        }
+    }
+}
